f47ea7d47c34cfa5ba1c56529bff21f7e294b257
jaewoo <202017499@jbnu.ac.kr>
2025-01-21 23:40:54 +0900

    0121

---
+++ 01_21/2875.py
@@ -0,0 +1,21 @@
+# 여자:남자 = 2:1
+# 여자 N, 남자 M, 인턴쉽 K
+
+import sys
+input = sys.stdin.readline
+
+
+N,M,k = list(map(int, input().strip().split()))
+
+f = N // 2
+m = M
+
+def boo(l):
+    if N+M-k >= (l * 2) + l:
+        print(l)
+    else:
+        l -= 1
+        boo(l)
+
+max_teams = min(f, m)
+boo(max_teams)
