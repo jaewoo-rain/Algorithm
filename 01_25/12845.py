21f96c816af1ea82d68eff04ba4524894f649830
jaewoo <202017499@jbnu.ac.kr>
2025-01-25 23:36:56 +0900

    0125

---
+++ 01_25/12845.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+n_list = list(map(int,input().strip().split()))
+
+max_level = max(n_list)
+
+# 최대값 하나 제거
+n_list.remove(max_level)
+
+result = 0
+for level in n_list:
+    result += max_level+level
+
+print(result)
